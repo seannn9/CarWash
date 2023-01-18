279730be06ec5e374b1a5c474599172e99413337
seannn9 <naeskirlu9@gmail.com>
2023-01-19 00:28:44 +0800

    v1.0
    - Added all necessary functionalities

--- src/CarWash.java
+++ src/CarWash.java
@@ -1,9 +1,8 @@
 
 public class CarWash {
-	private double price;
-	private int type;
-	private int level;
-	private String car;
+	private double price, change;
+	private int type, level;
+	private String car, lvl;
 	
 	CarWash(int type, int level) {
 		this.type = type;
@@ -11,37 +10,49 @@ public class CarWash {
 	}
 	
 	public double getPrice() {
-		if (type == 1) {
-			switch(level) {
-				case 1:
-					price = 250;
-					break;
-				case 2:
-					price = 500;
-					break;
-				case 3:
-					price = 750;
-					break;
-			}
-		} 
-		else if (type == 2) {
-			if (level == 1) {
-				price = 500;
-			} else if (level == 2) {
-				price = 750;
-			}
-		}
+		// 2D array to access prices easier depending on the level of the carwash and the type of car
+		int[][] prices = {{250, 500, 750},
+							{500, 750, 1000},
+							{750, 1000, 1250}
+							};
+		price = prices[level-1][type-1];
 		return price;
+	}
+
+	public double getChange(double pay) {
+		change = pay-price;
+		return change;
 	}
 	
 	public String getType() {
 		if (type == 1) {
 			car = "Sedan";
 		} else if (type == 2) {
-			car = "Pick-up Truck";
-		} else {
 			car = "SUV";
+		} else {
+			car = "Van";
 		}
 		return car;
+	}
+
+	public String getLevel() {
+		if (level == 1) {
+			lvl = "Normal Wash";
+		} else if (level == 2) {
+			lvl = "Intermediate Wash";
+		} else {
+			lvl = "Full-service Wash";
+		}
+		return lvl;
+	}
+
+	public void printReceipt(double pay) {
+		System.out.println();
+		System.out.println("\tReceipt:");
+		System.out.println("Type of Car: " + car);
+		System.out.println("Level of Cleaning: " + lvl);
+		System.out.println("Price: " + price);
+		System.out.println("Payment amount: " + pay);
+		System.out.println("Change: " + change);
 	}
 }
--- src/Main.java
+++ src/Main.java
@@ -6,11 +6,12 @@ public class Main {
 		Scanner in = new Scanner(System.in);
 		int type, level;
 		boolean run = true;
+		double pay;
 		
 		while (run) {
 			System.out.println("\tWelcome to our Car Wash!");
 			do {
-				System.out.print("What type of Car do you have?\n1 - Sedan\n2 - Pick-up Truck\n3 - Sedan\nyour car: ");
+				System.out.print("What type of Car do you have?\n1 - Sedan\n2 - SUV\n3 - Van\nyour car: ");
 				type = in.nextInt();
 			} while (type < 1 || type > 3);
 			
@@ -21,20 +22,35 @@ public class Main {
 						+ "\n2 - Intermediate Wash : (Exterior + Vaccuming Interior)\n3 - Full-service Wash : (Exterior + Full Interior)\nlevel: ");
 				level = in.nextInt();
 			} while (level < 1 || level > 3);
+
+			CarWash car = new CarWash(type, level);
+
+			System.out.println("\nProceeding to payment...");
+			System.out.println("The price is: " + car.getPrice());
+			do {
+				System.out.print("Enter your payment amount: ");
+				pay = in.nextDouble();
+				if (pay < car.getPrice()) {
+					System.out.println("Insufficient amount");
+				}
+			} while (pay < car.getPrice());
 			
-			CarWash car1 = new CarWash(type, level);
-			
-			System.out.print("Do you want to use the car wash again? y/n: ");
+			car.getChange(pay);
+			car.getType();
+			car.getLevel();
+			car.printReceipt(pay);
+
+
+			System.out.print("\nDo you want to use the car wash again? y/n: ");
 			char ans = in.next().toLowerCase().charAt(0);
 			if (ans == 'y') {
 				System.out.println();
 				continue;
 			} else if (ans == 'n'){
+				System.out.println("Thank you for trusting our car wash. Please come again!");
 				run = false;
 			}
 		}
-		
-		
+		in.close();
 	}
-
 }
