bbd648011534d734df3694a7752ad1e7b2819158
seannn9 <naeskirlu9@gmail.com>
2023-01-19 22:00:49 +0800

    v2.0
    - Overhauled

--- src/CarWash.java
+++ src/CarWash.java
@@ -4,11 +4,13 @@ public class CarWash {
 	private int type, level;
 	private String car, lvl;
 	
+	// constructor that takes in the type and level of the car as parameter and makes it available for the class
 	CarWash(int type, int level) {
 		this.type = type;
 		this.level = level;
 	}
 	
+	// method for getting the price from the list (depending on type and level) and returning it
 	public double getPrice() {
 		// 2D array to access prices easier depending on the level of the carwash and the type of car
 		int[][] prices = {{250, 500, 750},
@@ -19,11 +21,13 @@ public class CarWash {
 		return price;
 	}
 
+	// method for calulating change and returning it
 	public double getChange(double pay) {
 		change = pay-price;
 		return (double)change;
 	}
 	
+	// method for identifying the type of car and returning it
 	public String getType() {
 		if (type == 1) {
 			car = "Sedan";
@@ -35,6 +39,7 @@ public class CarWash {
 		return car;
 	}
 
+	// method for identifying level of car and returning it
 	public String getLevel() {
 		if (level == 1) {
 			lvl = "Normal Wash";
@@ -46,6 +51,7 @@ public class CarWash {
 		return lvl;
 	}
 
+	// method for printing the receipt after completing transaction
 	public void printReceipt(double pay) {
 		System.out.println();
 		System.out.println("\tReceipt:");
--- src/Main.java
+++ src/Main.java
@@ -4,35 +4,26 @@ import java.util.Scanner;
 public class Main {
 	static Scanner in = new Scanner(System.in);
 	public static void main(String[] args) {
-		int type = 0, level = 0;
+		int type, level;
 		double pay = 0.0;
-		char ans;		
-		do {
+		boolean run = true;	
+		String typePrompt = "What type of Car do you have?\n1 - Sedan\n2 - SUV\n3 - Van\nyour car: ";
+		String lvlPrompt = "What level of cleaning do you want for your vehicle?\n1 - Basic Wash : (Exterior only)"
+			+ "\n2 - Intermediate Wash : (Exterior + Vaccuming Interior)\n3 - Full-service Wash : (Exterior + Full Interior)\nlevel: ";
+
+		while (run) {
 			System.out.println("\tWelcome to our Car Wash!");
-			
+
 			// call getInput and pass prompt to get input on type and level
-			type = getInput("What type of Car do you have?\n1 - Sedan\n2 - SUV\n3 - Van\nyour car: ");
+			type = getInput(typePrompt);
 			System.out.println();
-			level = getInput("What level of cleaning do you want for your vehicle?\n1 - Basic Wash : (Exterior only)"
-			+ "\n2 - Intermediate Wash : (Exterior + Vaccuming Interior)\n3 - Full-service Wash : (Exterior + Full Interior)\nlevel: ");
+			level = getInput(lvlPrompt);
 
 			// Created a CarWash object that takes the type and level argument
 			CarWash car = new CarWash(type, level);
 
-			System.out.println("\nProceeding to payment...");
-			System.out.println("The price is: " + car.getPrice());
-			do {
-				System.out.print("Enter your payment amount: ");
-				try {
-					pay = in.nextDouble();
-				if (pay < car.getPrice()) {
-					System.out.println("Insufficient amount, please try again");
-				}
-				} catch (InputMismatchException e) {
-					System.out.println("Invalid input, please enter a valid amount");
-					in.nextLine();
-				}
-			} while (pay < car.getPrice());
+			// call processPayment to accept payment from user
+			pay = processPayment(car.getPrice());
 			
 			// call required methods from CarWash class
 			car.getChange(pay); 
@@ -40,16 +31,9 @@ public class Main {
 			car.getLevel(); 
 			car.printReceipt(pay); 
 
-			System.out.print("\nDo you want to use the car wash again? y/n: ");
-			ans = in.next().toLowerCase().charAt(0);
-			if (ans == 'y') {
-				System.out.println();
-				continue;
-			} else if (ans == 'n'){
-				System.out.println("\nThank you for trusting our car wash. Please come again!\n");
-				break;
-			}
-		} while (ans == 'y');
+			// call tryAgain method to ask if the user wants to go again or not and assing it to the run variable in the while loop
+			run = tryAgain();
+		}
 		in.close();
 	}
 
@@ -64,13 +48,57 @@ public class Main {
 				if (num >= 1 && num <= 3) { // only accept between 1 and 3
 					valid = true; // set valid as true resulting in !true which is false and exit the loop
 				} else {
-					System.out.println("Ivalid Input, please enter a number between 1-3");
+					System.out.println("\nIvalid Input, please enter a number between 1-3\n");
 				}
 			} catch (InputMismatchException e) {
-				System.out.println("Invalid Input, please enter a number between 1-3");
+				System.out.println("\nInvalid Input, please enter a number between 1-3\n");
 				in.nextLine();
 			}
 		}
 		return num;
 	}
+
+	// method for getting payment from user that takes in price variable from the CarWash class
+	public static double processPayment(double price) {
+		double pay = 0.0;
+		System.out.println("\nProceeding to payment...");
+			System.out.println("The price is: " + price);
+			do {
+				System.out.print("Enter your payment amount: ");
+				try {
+					pay = in.nextDouble();
+				if (pay < price) {
+					System.out.println("\nInsufficient amount, please try again");
+				}
+				} catch (InputMismatchException e) {
+					System.out.println("\nInvalid input, please enter a valid amount");
+					in.nextLine();
+				}
+			} while (pay < price);
+		return pay;
+	}
+
+	// method for getting decision from user if they want to go again
+	public static boolean tryAgain() {
+		String ans;
+		boolean run = true;
+		boolean tryAgain = true;
+		while (run) {
+			System.out.print("\nDo you want to use the car wash again? y/n: ");
+			ans = in.next().toLowerCase();
+			in.nextLine();
+			if (ans.equals("y")) {
+				System.out.println();
+				run = false;
+				tryAgain = true;
+			} else if (ans.equals("n")){
+				System.out.println("\nThank you for trusting our car wash. Please come again!\n");
+				run = false;
+				tryAgain = false;
+			} else {
+				System.out.println("\nInvalid input, please enter y/n");
+			}
+		}
+		return tryAgain;
+	} 
 } 
